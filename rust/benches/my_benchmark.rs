098db19eb18b513e191307da8958b4a0f3ada49c
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-09-05 01:11:01 +0200

    Extended benchmark tests to random gen

--- rust/benches/my_benchmark.rs
+++ rust/benches/my_benchmark.rs
@@ -2,10 +2,17 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pixelflut::packet::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("packet_build", |b| {
+    c.bench_function("packet_build_nonrandom", |b| {
         let mut packet_to_build = Packet::new(8);
         b.iter(|| {
             packet_to_build.add_string("███████████████");
+            packet_to_build.reset()
+        })
+    });
+    c.bench_function("packet_build_random", |b| {
+        let mut packet_to_build = Packet::new(8);
+        b.iter(|| {
+            packet_to_build.add_string("               ");
             packet_to_build.reset()
         })
     });

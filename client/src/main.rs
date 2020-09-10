b207655b020c17805e6860da3411a45cbf23e72c
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-09-10 03:57:22 +0200

    Gerenal color area and noise consumer

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,37 +1,91 @@
-use pixelflut::alphabet_bitarr::BitArrAlphabet;
+use pixelflut::alphabet_bitarr::BitMapAlphabet;
 use pixelflut::packet::*;
 use std::net::TcpStream;
 use std::time::Instant;
 
 const SERVER_IP: &str = "127.0.0.1:1337";
+//const SERVER_IP: &str = "127.0.0.1:3333";
 //const SERVER_IP: &str = "192.168.0.96:1337";
 //const SERVER_IP:&str = "192.168.0.161:1337";
 
+const RUNS: u32 = 10;
+
 // https://i0.wp.com/luckyresistor.me/wp-content/uploads/2015/09/font1.png?ssl=1
 
-fn send_data(to_send: String) {
+fn connect() -> TcpStream {
     let mut stream = TcpStream::connect(SERVER_IP).expect("Failed to connect");
     println!("Successfully connected to server {}", SERVER_IP);
-    let mut packet_to_build = Packet::<BitArrAlphabet>::new(8);
-    loop {
+    return stream;
+}
+
+fn send_data(to_send: String) {
+    let mut stream = connect();
+    let mut packet_to_build = Packet::<BitMapAlphabet>::new(8);
+    for _ in 0..RUNS {
+        //loop {
         let now = Instant::now();
         packet_to_build.add_string(&to_send);
         let build_took = now.elapsed();
         // Send and reset
         let _ = packet_to_build.write(&mut stream);
         packet_to_build.reset();
+        let total_took = now.elapsed();
         println!(
-            "Build took {}μs out of {}μs",
+            "Packet build took {}μs out of {}μs ({:.2}%)",
             build_took.as_micros(),
-            now.elapsed().as_micros()
+            now.elapsed().as_micros(),
+            (build_took.as_nanos() as f64 / total_took.as_nanos() as f64) * 100f64
         );
+    }
+}
+
+fn send_noise(start_x: u32, start_y: u32, end_x: u32, end_y: u32) {
+    let mut stream = connect();
+    let mut packet_to_build = Packet::<BitMapAlphabet>::new(8);
+    packet_to_build.add_noise(start_x, start_y, end_x, end_y);
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        packet_to_build.recalculate_noise();
+        let build_took = now.elapsed();
+        // Send and reset
+        let _ = packet_to_build.write(&mut stream);
+        let total_took = now.elapsed();
+        println!(
+            "Packet build took {}μs out of {}μs ({:.2}%)",
+            build_took.as_micros(),
+            now.elapsed().as_micros(),
+            (build_took.as_nanos() as f64 / total_took.as_nanos() as f64) * 100f64
+        );
+    }
+}
+
+fn create_and_consume(x: u32, y: u32, delay_ms: u128, chunks: u32) {
+    let mut stream = connect();
+    let mut packet_to_build = Packet::<BitMapAlphabet>::new(8);
+    for i in 0..chunks {
+        packet_to_build.add_area(i * x, 0, i * x + x, y, 0x123456);
+    }
+    let mut last_consume = Instant::now();
+    let size = x * y;
+    let mut i = chunks;
+    while i > 0 {
+        if last_consume.elapsed().as_millis() >= delay_ms {
+            println!("Extending with {}px", size);
+            packet_to_build.extend_noise(size);
+            last_consume = Instant::now();
+            i -= 1;
+        }
+        packet_to_build.recalculate_noise();
+        let _ = packet_to_build.write(&mut stream);
     }
 }
 
 fn main() {
     let input_arg = std::env::args().nth(1).expect("no input given");
 
-    send_data(input_arg);
+    // send_data(input_arg);
+    // send_noise(0, 0, 500, 500);
+    create_and_consume(50, 50, 1000, 20);
 
     println!("Terminated.");
 }
--- client/src/packet.rs
+++ client/src/packet.rs
@@ -1,8 +1,5 @@
 use crate::alphabet::Alphabet;
-use crate::alphabet_bitarr::BitArrAlphabet;
-use crate::alphabet_fast::BinarySearchAlphabet;
 use io::Write;
-use numtoa::NumToA;
 use std::{io, net::TcpStream};
 
 const SCREEN_WIDTH: u32 = 1600;
@@ -14,7 +11,8 @@ struct Pixel<'t> {
 }
 
 // const SCREEN_DIM_MAX_LEN: usize = 4;
-const COMMAND_SIZE: usize = 19; // "PX XXXX YYY CCCCCC\n"
+const MIN_COMMAND_SIZE: usize = 14; // "PX X Y CCCCCC\n"
+const MAX_COMMAND_SIZE: usize = 20; // "PX XXXX YYYY CCCCCC\n"
 
 pub struct Packet<A>
 where
@@ -25,6 +23,8 @@ where
     xoffset: u32,
     yoffset: u32,
     scale: u32,
+    noise_start: usize,
+    noise_end: usize,
     alphabet: A,
     //buf: [u8; SCREEN_DIM_MAX_LEN],
 }
@@ -35,61 +35,140 @@ where
 {
     pub fn new(scale: u32) -> Packet<A> {
         Packet {
-            data: vec![0; COMMAND_SIZE * SCREEN_HEIGHT as usize * SCREEN_WIDTH as usize],
+            data: vec![0; MAX_COMMAND_SIZE * SCREEN_HEIGHT as usize * SCREEN_WIDTH as usize],
             data_i: 0,
             xoffset: 0,
             yoffset: 0,
             scale,
+            noise_start: 0,
+            noise_end: 0,
             alphabet: Alphabet::new(),
             //buf: [0 as u8; SCREEN_DIM_MAX_LEN],
         }
     }
 
-    fn write_and_update_index(&mut self, slice: &[u8], i: &mut usize) {
+    fn write_slice_and_update_index(&mut self, slice: &[u8], i: &mut usize) {
         let len = slice.len();
         self.data[*i..*i + len].copy_from_slice(slice);
         *i += len;
     }
 
+    fn write_color_and_update_index(&mut self, rgb: u32, i: &mut usize) {
+        for shift in 0..6 {
+            let mut val = ((rgb >> (shift * 4)) & 0xf) + 48;
+            if val > 57 {
+                val += 7;
+            }
+            self.data[*i + shift] = val as u8;
+        }
+        *i += 6;
+    }
+
     fn write_num_and_update_index(&mut self, num: usize, i: &mut usize) {
-        // 1234 / 1000 = 1
-        // 1234 % 1000 = 234 / 100 = 2
-        let th = (num / 1000) as u8;
-        let h = ((num % 1000) / 100) as u8;
-        let te = ((num % 100) / 10) as u8;
-        let o = (num % 10) as u8;
-        if th != 0 {
-            self.data[*i] = th + 48;
-            self.data[*i + 1] = h + 48;
-            self.data[*i + 2] = te + 48;
+        let thousands = (num / 1000) as u8;
+        let hundreds = ((num % 1000) / 100) as u8;
+        let tens = ((num % 100) / 10) as u8;
+        let ones = (num % 10) as u8;
+        if thousands != 0 {
+            self.data[*i] = thousands + 48;
+            self.data[*i + 1] = hundreds + 48;
+            self.data[*i + 2] = tens + 48;
             *i += 3;
-        } else if h != 0 {
-            self.data[*i] = h + 48;
-            self.data[*i + 1] = te + 48;
+        } else if hundreds != 0 {
+            self.data[*i] = hundreds + 48;
+            self.data[*i + 1] = tens + 48;
             *i += 2;
-        } else if te != 0 {
-            self.data[*i] = te + 48;
+        } else if tens != 0 {
+            self.data[*i] = tens + 48;
             *i += 1;
         }
-        self.data[*i] = o + 48;
+        self.data[*i] = ones + 48;
         *i += 1;
     }
 
-    //#[inline(never)]
-    fn add_pixel(&mut self, pixel: Pixel) {
+    fn get_rand_color(&mut self) -> u32 {
+        return rand::random::<u32>();
+    }
+
+    fn get_next_command(&mut self, start: &mut usize) {
+        // From min to max command index (looking for end of command)
+        for j in MIN_COMMAND_SIZE - 1..MAX_COMMAND_SIZE - 1 {
+            if self.data[*start + j] == '\n' as u8 {
+                *start += j + 1;
+                return;
+            }
+        }
+    }
+
+    fn recalculate_pixel_noise(&mut self, i: &mut usize) {
+        self.get_next_command(i);
+        *i -= 7;
+        let rgb = self.get_rand_color();
+        self.write_color_and_update_index(rgb, i);
+        *i += 1;
+    }
+
+    pub fn recalculate_noise(&mut self) {
+        let mut i = self.noise_start;
+        while i < self.noise_end {
+            self.recalculate_pixel_noise(&mut i);
+        }
+    }
+
+    pub fn add_area(&mut self, start_x: u32, start_y: u32, end_x: u32, end_y: u32, color: u32) {
         let mut i = self.data_i;
-        self.write_and_update_index(b"PX ", &mut i);
-        // let tmp = pixel.x.numtoa(10, &mut self.data[i..]);
-        // i += tmp.len();
-        self.write_num_and_update_index(pixel.x as usize, &mut i);
-        self.write_and_update_index(b" ", &mut i);
-        // let tmp = pixel.y.numtoa(10, &mut self.data[i..]);
-        // i += tmp.len();
-        self.write_num_and_update_index(pixel.y as usize, &mut i);
-        self.write_and_update_index(b" ", &mut i);
-        self.write_and_update_index(pixel.color, &mut i);
-        self.write_and_update_index(b"\n", &mut i);
+        for x in start_x..end_x {
+            for y in start_y..end_y {
+                self.add_pixel(x, y, color, &mut i);
+            }
+        }
         self.data_i = i;
+    }
+
+    pub fn add_noise(&mut self, start_x: u32, start_y: u32, end_x: u32, end_y: u32) {
+        let mut i = self.noise_end;
+        for x in start_x..end_x {
+            for y in start_y..end_y {
+                let rgb = self.get_rand_color();
+                self.add_pixel(x, y, rgb, &mut i);
+            }
+        }
+
+        self.data_i += i - self.noise_end;
+        self.noise_end = i;
+    }
+
+    pub fn extend_noise(&mut self, pixels: u32) {
+        println!(
+            "Extending {}px from {} in {}",
+            pixels, self.noise_start, self.data_i
+        );
+        let mut i = self.noise_end;
+        for j in 0..pixels {
+            self.get_next_command(&mut i);
+        }
+        if i <= self.data_i {
+            self.noise_end = i;
+        } else {
+            panic!("Trying to extend noise beyond commands")
+        }
+    }
+
+    fn add_pixel_no_overwrite(&mut self, x: u32, y: u32, rgb: u32) {
+        let mut i = self.data_i;
+        self.add_pixel(x, y, rgb, &mut i);
+        self.data_i = i;
+    }
+
+    //#[inline(never)]
+    fn add_pixel(&mut self, x: u32, y: u32, rgb: u32, i: &mut usize) {
+        self.write_slice_and_update_index(b"PX ", i);
+        self.write_num_and_update_index(x as usize, i);
+        self.write_slice_and_update_index(b" ", i);
+        self.write_num_and_update_index(y as usize, i);
+        self.write_slice_and_update_index(b" ", i);
+        self.write_color_and_update_index(rgb, i);
+        self.write_slice_and_update_index(b"\n", i);
     }
 
     pub fn add_letter(&mut self, c: char) {
@@ -97,6 +176,7 @@ where
         let scale_x: u32 = self.scale;
         let width: u32 = 8;
         let height: u32 = 8;
+        let mut data_i = self.data_i;
         //let array = char_to_pixel_positions(c).expect(&format!("Invalid char: {}", c));
         for y in 0..height {
             //print!("{}\n", termion::style::Reset);
@@ -106,23 +186,20 @@ where
                 let array_contains_i = self.alphabet.is_pixel_in_char(c, i as u8);
                 for zy in 0..scale_y {
                     for zx in 0..scale_x {
-                        let color: &mut [u8] = &mut ['0' as u8; 6];
                         let x = zx + self.xoffset + x * scale_x;
                         let y = zy + self.yoffset + y * scale_y;
+                        let mut rgb = 0x000000;
                         if !array_contains_i {
-                            let rand_num = rand::random::<u32>();
-                            (rand_num & 0xff).numtoa(16, &mut color[0..2]);
-                            ((rand_num >> 8) & 0xff).numtoa(16, &mut color[2..4]);
-                            ((rand_num >> 8 * 2) & 0xff).numtoa(16, &mut color[4..6]);
+                            rgb = self.get_rand_color();
                         }
-                        let pixel = Pixel { x, y, color };
-                        self.add_pixel(pixel)
-                        //print!(" ");
+                        //println!("Color: {:x}", rgb);
+                        self.add_pixel(x, y, rgb, &mut data_i);
                     }
                 }
             }
             //print!("{}\n", termion::style::Reset)
         }
+        self.data_i = data_i;
         self.xoffset += width * self.scale;
         if self.xoffset + width * self.scale > SCREEN_WIDTH {
             self.xoffset = 0;
@@ -152,11 +229,13 @@ where
 }
 #[cfg(test)]
 mod tests {
+    use crate::alphabet_bitarr::BitMapAlphabet;
+
     use super::*;
 
     #[test]
     fn test_non_random() {
-        let mut packet_to_build = Packet::<BitArrAlphabet>::new(8);
+        let mut packet_to_build = Packet::<BitMapAlphabet>::new(8);
         packet_to_build.add_string("▓▓▓▓▓▓▓▓▓▓▓");
 
         let mut sum = 0u64;
